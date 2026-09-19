798fa34402445ec7e35b8ded35f48ea7fe668aa5
Govar <govardhangowda2007@gmail.com>
2026-09-19 07:57:30 +0530

    learnt arithematic operators

---
+++ arithmatic_operators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+  int x=2;
+  x++;
+  std:: cout <<x<<"\n";
+
+  double y=3;
+  double d=y/x;
+  std:: cout<<d<<"\n";
+
+  int f= y;
+  double r= (f % x);
+  std::cout<<r<<"\n";
+
+  double m= y*x;
+  std::cout<<m<<"\n";
+
+  int s= y-x;
+  std::cout<<d<<"\n";
+
+
+  return 0;
+}
